4368f06ff7507ceb308c5bc57899e61a10d81ab1
Swepool <36674091+Swepool@users.noreply.github.com>
2022-01-12 22:54:53 +0100

    added

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,17 @@ module.exports = {
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
+        'gatsby-plugin-emotion',
         'gatsby-plugin-netlify-cms',
-        `gatsby-transformer-remark`
+        `gatsby-transformer-remark`,
+        'gatsby-plugin-sitemap',
+        'gatsby-plugin-mdx',
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/blog`,
+                name: `markdown-pages`,
+            },
+        },
     ]
 }
