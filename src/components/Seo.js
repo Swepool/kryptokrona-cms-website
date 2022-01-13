0e2cbff107ca1dd5897ede1f5883a187e8831d65
Swepool <36674091+Swepool@users.noreply.github.com>
2022-01-13 17:22:43 +0100

    Seo and Https on links

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -7,12 +7,10 @@ const Seo = ({title='Kryptokrona'}) => {
             <html lang="en"/>
             <title>{title}</title>
             <meta name="Kryptokrona" content="Kryptokrona"/>
-            <meta name="description" content="Hugin is a messaging service where messages are stored on the kryptokrona blockchain. Your
-                        messages are secured with industry leading encryption so that only you and your chat partner have
-                        the possibility to read them."/>
+            <meta name="description" content="Kryptokrona exists to secure and simplify our future economic system. Sending and receiving money should not be expensive or slow. We work with open source code that allows you to be involved and improve the money of the future."/>
             <meta name="theme-color" content="#1d1d1d"/>
             <meta name="robots" content="index, follow"/>
-            <link rel="canonical" href="http://Kryptokrona.org/" />
+            <link rel="canonical" href="https://Kryptokrona.org/" />
             <link rel="icon" src="../images/favicon.ico" type="image/x-icon"/>
             <script defer data-domain="kryptokrona.org" src="https://plausible.io/js/plausible.js"/>
             <meta property="og:image" content="https://user-images.githubusercontent.com/36674091/148423512-2c019bd6-6b32-45ce-b380-100632b00f0f.png" />
