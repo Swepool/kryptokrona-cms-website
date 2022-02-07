2bdefbaa7dc91f8b5218210375b09bcd562c8547
Swepool <36674091+Swepool@users.noreply.github.com>
2022-02-07 14:11:59 +0100

    Changes to theme

---
+++ src/theme/colors.js
@@ -0,0 +1,16 @@
+
+//Color, import to color.js to component and use ${color.yourcolor}
+
+export const color = {
+    black: "#000000",
+    background:"#1d1d1d",
+    navbarBG: "rgba(29,29,29,0.5)",
+    white: "#ffffff",
+    white80: "rgba(255,255,255,0.8)",
+    white60: "rgba(255,255,255,0.5)",
+    green: "#5ff281",
+    red: "#F25F61",
+    border: "",
+    divide: "#FFFFFF26",
+
+}
