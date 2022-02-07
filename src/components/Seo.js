632800015f56812a86180d2b05bed8793e15345b
Swepool <36674091+Swepool@users.noreply.github.com>
2022-02-07 14:09:49 +0100

    Removed static canonical

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -10,7 +10,6 @@ const Seo = ({title='Kryptokrona'}) => {
             <meta name="description" content="Kryptokrona exists to secure and simplify our future economic system. Sending and receiving money should not be expensive or slow. We work with open source code that allows you to be involved and improve the money of the future."/>
             <meta name="theme-color" content="#1d1d1d"/>
             <meta name="robots" content="index, follow"/>
-            <link rel="canonical" href="https://Kryptokrona.org/" />
             <link rel="icon" src="../images/favicon.ico" type="image/x-icon"/>
             <script defer data-domain="kryptokrona.org" src="https://plausible.io/js/plausible.js"/>
             <meta property="og:image" content="https://user-images.githubusercontent.com/36674091/148423512-2c019bd6-6b32-45ce-b380-100632b00f0f.png" />
