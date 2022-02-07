ee88d7bb65ad6aefef4eb66d81901a2e3caeae34
Swepool <36674091+Swepool@users.noreply.github.com>
2022-02-07 14:49:16 +0100

    Search verification

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -10,6 +10,7 @@ const Seo = ({title='Kryptokrona'}) => {
             <meta name="description" content="Kryptokrona exists to secure and simplify our future economic system. Sending and receiving money should not be expensive or slow. We work with open source code that allows you to be involved and improve the money of the future."/>
             <meta name="theme-color" content="#1d1d1d"/>
             <meta name="robots" content="index, follow"/>
+            <meta name="google-site-verification" content="HYv_ge8cXwaJ2eSQzw2hnuYJUzw0_J0tlxHPHsy8-bM" />
             <link rel="icon" src="../images/favicon.ico" type="image/x-icon"/>
             <script defer data-domain="kryptokrona.org" src="https://plausible.io/js/plausible.js"/>
             <meta property="og:image" content="https://user-images.githubusercontent.com/36674091/148423512-2c019bd6-6b32-45ce-b380-100632b00f0f.png" />
